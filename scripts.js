342097997298b9994e63ec60d18fa2bbc62dca6a
Daniel Wiet <danwiet@gmail.com>
2021-08-15 19:13:27 -0500

    add rps game

--- scripts.js
+++ scripts.js
@@ -8,10 +8,10 @@ function closeNav() {
 
 function humanOrRobot() {
   let robot = confirm("Are you human? Ok for yes, cancel for no");
-  if (robot == true) {
+  if (robot === true) {
     //If the test condition 1 is TRUE then these it will check for test condition 2
     let rUSure = confirm("Are you certain? OK for yes, cancel for no");
-    if (rUSure == false) {
+    if (rUSure === false) {
       //If the test condition 2 is TRUE then these statements will be executed
       alert("Only humans question their own existence");
     } else {
@@ -23,3 +23,68 @@ function humanOrRobot() {
     alert("Honest robots also welcome");
   }
 }
+
+const getUserChoice = () => {
+  let userInput = window.prompt("Choose either rock, paper, or scissors");
+  userInput = userInput.toLowerCase();
+  if (
+    userInput === "laser" ||
+    userInput === "rock" ||
+    userInput === "paper" ||
+    userInput === "scissors"
+  ) {
+    return userInput;
+  } else {
+    console.log("Error!");
+  }
+};
+
+const getComputerChoice = () => {
+  let randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
+};
+
+const determineWinner = (userChoice, computerChoice) => {
+  if (userChoice === "laser") {
+    alert("You played the secret weapon and obliterated your opponent");
+  }
+  if (userChoice === computerChoice) {
+    alert ("Game is a tie");
+  }
+  if (userChoice == "rock") {
+    if (computerChoice === "paper") {
+      alert ("The computer won!");
+    } else {
+      alert ("You won!");
+    }
+  }
+  if (userChoice === "paper") {
+    if (computerChoice === "scissors") {
+      alert ("The computer won!");
+    } else {
+      alert ("You won!");
+    }
+  }
+  if (userChoice === "scissors") {
+    if (computerChoice === "rock") {
+      alert ("The computer won!");
+    } else {
+      alert ("You won!");
+    }
+  }
+};
+
+const playGame = () => {
+  const userChoice = getUserChoice();
+  const computerChoice = getComputerChoice();
+  alert("You chose " + userChoice);
+  alert("The computer chose " + computerChoice);
+  alert(determineWinner(userChoice, computerChoice));
+};
